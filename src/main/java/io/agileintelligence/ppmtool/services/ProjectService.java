40ab1778015cd6e1b7d695b8a71c9fea645c0b42
Wojciech Dudek <wdudek82@gmail.com>
2018-11-19 23:33:38 +0100

    Added api endpint that allows creation of new projects

---
+++ src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -0,0 +1,25 @@
+package io.agileintelligence.ppmtool.services;
+
+import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public Project saveOrUpdateProject(Project project) {
+
+        // Logic
+
+        return projectRepository.save(project);
+    }
+
+}
